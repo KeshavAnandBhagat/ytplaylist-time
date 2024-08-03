f4ff6b441092af5fe82725e2e1c15758a84339df
Keshav Anand Bhagat <62502378+KeshavAnandBhagat@users.noreply.github.com>
2024-08-03 16:59:58 +0530

    Update app.py 3

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 from flask import Flask, Response, request, render_template
-import datetime
 import isodate
 import json
 import re
@@ -10,10 +9,8 @@ import os
 APIS = os.environ['APIS'].strip('][').split(',')
 
 URL1 = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&fields=items/contentDetails/videoId,nextPageToken&key={}&playlistId={}&pageToken='
-URL2 = 'https://www.googleapis.com/youtube/v3/videos?&part=contentDetails&id={}&key={}&fields=items/contentDetails/duration'
+URL2 = 'https://www.googleapis.com/youtube/v3/videos?&part=contentDetails,snippet,statistics&id={}&key={}&fields=items(contentDetails/duration,snippet(title,publishedAt),statistics(viewCount,likeCount))'
 
-
-# To get the playlistId from the link
 def get_id(playlist_link):
     p = re.compile('^([\S]+list=)?([\w_-]+)[\S]*$')
     m = p.match(playlist_link)
@@ -22,9 +19,7 @@ def get_id(playlist_link):
     else:
         return 'invalid_playlist_link'
 
-
-# To parse the datetime object into readable time
-def parse(a):
+def parse_duration(a):
     ts, td = a.seconds, a.days
     th, tr = divmod(ts, 3600)
     tm, ts = divmod(tr, 60)
@@ -41,16 +36,12 @@ def parse(a):
         ds = '0 seconds'
     return ds.strip().strip(',')
 
-
-# find if a time lies between two other times
 def todayAt(hr, min=0, sec=0, micros=0):
-    now = datetime.datetime.now()
+    now = datetime.now()
     return now.replace(hour=hr, minute=min, second=sec, microsecond=micros)
 
-
-# find out which time slice an time lies in, to decide which API key to use
 def find_time_slice():
-    timeNow = datetime.datetime.now()
+    timeNow = datetime.now()
     time_slice = 0
     if todayAt(0) <= timeNow < todayAt(4):
         time_slice = 1
@@ -64,99 +55,93 @@ def find_time_slice():
         time_slice = 5
     return time_slice
 
-
 app = Flask(__name__, static_url_path='/static')
-
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    if (request.method == 'GET'):
+    if request.method == 'GET':
         return render_template("home.html")
-
     else:
-
-        # get playlist link/id as input from the form
         playlist_link = request.form.get('search_string').strip()
         playlist_id = get_id(playlist_link)
 
-        # initializing variables
-        next_page = ''  # to hold next_page token, empty for first page
-        cnt = 0  # stores number of videos in playlist
-        a = timedelta(0)  # to store total length of playlist
+        next_page = ''
+        cnt = 0
+        total_duration = timedelta(0)
         tsl = find_time_slice()
-        display_text = []  # list to contain final text to be displayed, one item per line
+        display_text = []
 
         print(APIS[tsl])
-        # when we make requests, we get the response in pages of 50 items
-        # which we process one page at a time
+        video_details = []
+
         while True:
             vid_list = []
-
             try:
-                # make first request to get list of all video_id one page of response
                 print(URL1.format(APIS[tsl].strip("'"), playlist_id))
                 results = json.loads(requests.get(URL1.format(APIS[tsl].strip("'"), playlist_id) + next_page).text)
 
-                # add all ids to vid_list
                 for x in results['items']:
                     vid_list.append(x['contentDetails']['videoId'])
-
             except KeyError:
                 display_text = [results['error']['message']]
                 break
 
-            # now vid_list contains list of all videos in playlist one page of response
             url_list = ','.join(vid_list)
-            # updating counter
             cnt += len(vid_list)
 
             try:
-                # now to get the durations of all videos in url_list
                 op = json.loads(requests.get(URL2.format(url_list, APIS[tsl].strip("'"))).text)
-
-                # add all the durations to a
                 for x in op['items']:
-                    a += isodate.parse_duration(x['contentDetails']['duration'])
-
+                    duration = isodate.parse_duration(x['contentDetails']['duration'])
+                    total_duration += duration
+                    video_details.append({
+                        'title': x['snippet']['title'],
+                        'duration': duration,
+                        'views': x['statistics'].get('viewCount', 'N/A'),
+                        'likes': x['statistics'].get('likeCount', 'N/A'),
+                        'publishedAt': x['snippet']['publishedAt']
+                    })
             except KeyError:
                 display_text = [results['error']['message']]
                 break
 
-            # if 'nextPageToken' is not in results, it means it is the last page of the response
-            # otherwise, or if the cnt has not yet exceeded 500
             if 'nextPageToken' in results and cnt < 500:
                 next_page = results['nextPageToken']
             else:
                 if cnt >= 500:
                     display_text = ['No of videos limited to 500.']
                 display_text += [
-                    'No of videos : ' + str(cnt), 'Average length of video : ' + parse(a / cnt),
-                    'Total length of playlist : ' + parse(a), 'At 1.25x : ' + parse(a / 1.25),
-                    'At 1.50x : ' + parse(a / 1.5), 'At 1.75x : ' + parse(a / 1.75), 'At 2.00x : ' + parse(a / 2)
+                    'No of videos : ' + str(cnt), 'Average length of video : ' + parse_duration(total_duration / cnt),
+                    'Total length of playlist : ' + parse_duration(total_duration), 'At 1.25x : ' + parse_duration(total_duration / 1.25),
+                    'At 1.50x : ' + parse_duration(total_duration / 1.5), 'At 1.75x : ' + parse_duration(total_duration / 1.75),
+                    'At 2.00x : ' + parse_duration(total_duration / 2)
                 ]
                 break
 
-        return render_template("home.html", display_text=display_text)
+        sorted_video_details = sorted(video_details, key=lambda x: isodate.parse_duration(x['duration']))
 
+        display_text.append("Shortest Video: " + sorted_video_details[0]['title'])
+        display_text.append("Longest Video: " + sorted_video_details[-1]['title'])
+        display_text.append("Latest Video: " + max(video_details, key=lambda x: x['publishedAt'])['title'])
+        display_text.append("Oldest Video: " + min(video_details, key=lambda x: x['publishedAt'])['title'])
+
+        return render_template("home.html", display_text=display_text, video_details=video_details)
 
 @app.route("/healthz", methods=['GET', 'POST'])
 def healthz():
     return "Success", 200    
-    
-    
+
 @app.route('/.well-known/brave-rewards-verification.txt')
 def static_from_root_brave():
     return Response(
         'This is a Brave Rewards publisher verification file.\n\nDomain: ytplaylist-len.herokuapp.com\nToken: aae68b8a5242a8e5f0505ee6eaa406bd51edf0dc9a05294be196495df223385c',
         mimetype='text/plain')
 
-
 @app.route('/ads.txt')
 def static_from_root_google():
     return Response(
         'google.com, pub-8874895270666721, DIRECT, f08c47fec0942fa0',
         mimetype='text/plain')
-
 
 if __name__ == "__main__":
     app.run(use_reloader=True, debug=False)
